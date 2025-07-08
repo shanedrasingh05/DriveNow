bfaa07457dfd98fefb191c4c4732c4b9bc3ffe7e
Shanedra Singh <shanedrasingh89@gmail.com>
2025-07-08 23:40:05 +0530

    add file

--- client/src/components/CarCard.jsx
+++ client/src/components/CarCard.jsx
@@ -1,77 +1,77 @@
-// import React from 'react'
-// import {assets} from '../assets/assets'
+import React from 'react'
+import {assets} from '../assets/assets'
 
-// const CarCard = ({car}) => {
+const CarCard = ({car}) => {
 
-//     const currency = import.meta.env.VITE_CURRENCY;
+    const currency = import.meta.env.VITE_CURRENCY;
 
-//   return (
-//     <div className='group rounded-xl overflow-hidden shadow-xl hover:-translate-y-1 
-//     transition-all duration-500 cursor-pointer'>
+  return (
+    <div className='group rounded-xl overflow-hidden shadow-xl hover:-translate-y-1 
+    transition-all duration-500 cursor-pointer'>
 
-//         <div className='relative h-48 overflow-hidden'>
-//             <img src={car.image} alt="Car image" className='w-full h-full object-cover
-//             transition-transform duration-500 group-hover:scale-105' />
+        <div className='relative h-48 overflow-hidden'>
+            <img src={car.image} alt="Car image" className='w-full h-full object-cover
+            transition-transform duration-500 group-hover:scale-105' />
 
-//             {car.isAvailable && <p className='absolute top-5 left-4 bg-primary/90 text-white text-xs px-2.5 py-1 rounded-ffull'>
-//                 Available Now
-//             </p> }
-
-
-//                 <div className='absolute bottom-4 right-4 bg-black/80 backdrop-blur-sm text-white px-3 py-2 rounded-lg'>
-//                 <span className='font-semibold'>{currency}{car.PricePerDay}</span>
-//                 <span className='text-sm text-white/80'> / day</span>
-
-//                 </div>
-
-//         </div>
+            {car.isAvailable && <p className='absolute top-5 left-4 bg-primary/90 text-white text-xs px-2.5 py-1 rounded-ffull'>
+                Available Now
+            </p> }
 
 
-//         <div className='p-4 sm:p-5'>
+                <div className='absolute bottom-4 right-4 bg-black/80 backdrop-blur-sm text-white px-3 py-2 rounded-lg'>
+                <span className='font-semibold'>{currency}{car.PricePerDay}</span>
+                <span className='text-sm text-white/80'> / day</span>
 
-//             <div className='flex justify-between items-start mb-2'>
-//                 <div>
-//                     <h3 className='text-lg font-medium'>{car.brand} {car.model}</h3>
-//                     <p className='text-muted-foregraound text-sm'>{car.category} {car.year}</p>
-//                 </div>
+                </div>
 
-//             </div>
+        </div>
 
 
-//             <div className='mt-4 grid grid-cols-2 gap-y-2 text-gray-600'>
+        <div className='p-4 sm:p-5'>
 
-//                 <div className="flex items-center text-sm text-muted-foreground">
-//                     <img src={assets.users_icon} alt="" className='h-4 mr-2' />
-//                     <span>{car.seating_capacity} Seats</span>
+            <div className='flex justify-between items-start mb-2'>
+                <div>
+                    <h3 className='text-lg font-medium'>{car.brand} {car.model}</h3>
+                    <p className='text-muted-foregraound text-sm'>{car.category} {car.year}</p>
+                </div>
 
-//                 </div>
-
-//                 <div className="flex items-center text-sm text-muted-foreground">
-//                     <img src={assets.fuel_icon} alt="" className='h-4 mr-2' />
-//                     <span>{car.fuel_type} </span>
-
-//                 </div>
+            </div>
 
 
-//                 <div className="flex items-center text-sm text-muted-foreground">
-//                     <img src={assets.car_icon} alt="" className='h-4 mr-2' />
-//                     <span>{car.transmission} </span>
+            <div className='mt-4 grid grid-cols-2 gap-y-2 text-gray-600'>
 
-//                 </div>
+                <div className="flex items-center text-sm text-muted-foreground">
+                    <img src={assets.users_icon} alt="" className='h-4 mr-2' />
+                    <span>{car.seating_capacity} Seats</span>
 
-//                 <div className="flex items-center text-sm text-muted-foreground">
-//                     <img src={assets.location_icon} alt="" className='h-4 mr-2' />
-//                     <span>{car.location} </span>
+                </div>
 
-//                 </div>
+                <div className="flex items-center text-sm text-muted-foreground">
+                    <img src={assets.fuel_icon} alt="" className='h-4 mr-2' />
+                    <span>{car.fuel_type} </span>
 
-//             </div>
+                </div>
 
 
-//         </div>
+                <div className="flex items-center text-sm text-muted-foreground">
+                    <img src={assets.car_icon} alt="" className='h-4 mr-2' />
+                    <span>{car.transmission} </span>
+
+                </div>
+
+                <div className="flex items-center text-sm text-muted-foreground">
+                    <img src={assets.location_icon} alt="" className='h-4 mr-2' />
+                    <span>{car.location} </span>
+
+                </div>
+
+            </div>
+
+
+        </div>
       
-//     </div>
-//   )
-// }
+    </div>
+  )
+}
 
-// export default CarCard
+export default CarCard
