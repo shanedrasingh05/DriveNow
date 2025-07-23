ff6d902ea9d3076e66a53bc7b98fe3db81b519c0
Shanedra Singh <shanedrasingh89@gmail.com>
2025-07-23 23:33:43 +0530

    add file

--- client/src/pages/owner/ManageCars.jsx
+++ client/src/pages/owner/ManageCars.jsx
@@ -1,46 +1,46 @@
-// import React, { useEffect, useState } from 'react'
-// import { dummyCarData } from '../../assets/assets';
-// import Title from '../../components/owner/Title';
+import React, { useEffect, useState } from 'react'
+import { dummyCarData } from '../../assets/assets';
+import Title from '../../components/owner/Title';
 
 
-// const ManageCars = () => {
-//     const [cars, setCars] = useState([]);
+const ManageCars = () => {
+    const [cars, setCars] = useState([]);
 
-//     const fetchOwnerCars = async () =>{
-//       setCars(dummyCarData);
-//     }
+    const fetchOwnerCars = async () =>{
+      setCars(dummyCarData);
+    }
 
-//     useEffect(() => {
-//       fetchOwnerCars();
-//     }, [])
-
-
-//   return (
-
-//     <div className="px-4 pt-10 md:px-10 w-full">
-//       <Title
-//         title="Manage Cars"
-//         subtitle="View all listed cars, update their details, or remove them from the booking platform."
-//       />
-
-//       <div className="max-w-3xl w-full rounded-md overflow-hidden border border-borderColor mt-6">
-
-//         <table className="w-full border-collapse text-left text-sm text-gray-600">
-//           <thead className="text-gray-500">
-//             <tr>
-//               <th className="p-3 font-medium">Car</th>
-//               <th className="p-3 font-medium max-md:hidden">Category</th>
-//               <th className="p-3 font-medium">Price</th>
-//               <th className="p-3 font-medium max-md:hidden">Status</th> 
-//               <th className="p-3 font-medium">Actions</th>
-//             </tr>
-//           </thead>
-//         </table>
+    useEffect(() => {
+      fetchOwnerCars();
+    }, [])
 
 
-//       </div>
-//     </div>
-//   );
-// }
+  return (
 
-// export default ManageCars
+    <div className="px-4 pt-10 md:px-10 w-full">
+      <Title
+        title="Manage Cars"
+        subtitle="View all listed cars, update their details, or remove them from the booking platform."
+      />
+
+      <div className="max-w-3xl w-full rounded-md overflow-hidden border border-borderColor mt-6">
+
+        <table className="w-full border-collapse text-left text-sm text-gray-600">
+          <thead className="text-gray-500">
+            <tr>
+              <th className="p-3 font-medium">Car</th>
+              <th className="p-3 font-medium max-md:hidden">Category</th>
+              <th className="p-3 font-medium">Price</th>
+              <th className="p-3 font-medium max-md:hidden">Status</th> 
+              <th className="p-3 font-medium">Actions</th>
+            </tr>
+          </thead>
+        </table>
+
+
+      </div>
+    </div>
+  );
+}
+
+export default ManageCars
