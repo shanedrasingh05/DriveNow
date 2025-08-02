a0911f7a1194f14eee724505de2cbda5aa3f65eb
Shanedra Singh <shanedrasingh89@gmail.com>
2025-08-02 16:54:22 +0530

    add file

--- server/routes/ownerRoutes.js
+++ server/routes/ownerRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { protect } from "../middleware/auth.js";
 import { changeRoleToOwner } from "../controllers/ownerController.js";
+import upload from "../middleware/multer.js";
+import { addCar } from "../controllers/ownerController.js";
 
 const ownerRouter = express.Router();
 
 ownerRouter.post("/change-role", protect, changeRoleToOwner);
+ownerRouter.post("/add-car",upload.single("image"), protect, addCar);
 
 export default ownerRouter;
 
