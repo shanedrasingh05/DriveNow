ed4c8c902d58544f48e6912123ff1539a0cdab04
Shanedra Singh <shanedrasingh89@gmail.com>
2025-08-03 23:51:36 +0530

    add file

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -36,10 +36,92 @@ export const checkAvailabilityOfCar = async (req, res) => {
 
     res.json({ success: true, availableCars });
 
-    
+
 
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
   }
 };
+
+
+// API to Create Booking
+export const createBooking = async (req, res)=>{
+    try {
+        const {_id} = req.user;
+        const {car, pickupDate, returnDate} = req.body;
+
+        const isAvailable = await checkAvailability(
+          car,
+          pickupDate,
+          returnDate
+        );
+        if (!isAvailable) {
+          return res.json({ success: false, message: "Car is not available" });
+        }
+
+
+        const carData = await Car.findById(car);
+
+        // Calculate price based on pickupDate and returnDate
+        const picked = new Date(pickupDate);
+        const returned = new Date(returnDate);
+
+        const noOfDays = Math.ceil((returned - picked) / (1000 * 60 * 60 * 24));
+        const price = carData.pricePerDay * noOfDays;
+
+
+        await Booking.create({
+          car,
+          owner: carData.owner,
+          user: _id,
+          pickupDate,
+          returnDate,
+          price,
+        });
+
+        res.json({ success: true, message: "Booking Created" });
+
+
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
+
+
+// API to List User Bookings
+export const getUserBookings = async (req, res)=>{
+    try {
+        const {_id} = req.user;
+        const bookings = await Booking.find({ user: _id }).populate("car").sort({createdAt: -1});
+        res.json({ success: true, bookings});
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
+
+
+// API to get Owner Bookings
+export const getOwnerBookings = async (req, res)=>{
+    try {
+        if(req.user.role !== 'owner'){
+            return res.json({ success: false, message: "Unauthorized" })
+        }
+       const bookings = await Booking.find({ owner: req.user._id })
+         .populate("car user")
+         .select("-user.password")
+         .sort({ createdAt: -1 });
+          res.json({ success: true, bookings });
+
+
+        
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
